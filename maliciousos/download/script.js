af13b0517a82d3a23caf77eb757156a2d3f3f2a3
longlonger2022 <long_long2022@163.com>
2025-08-03 21:05:06 +0800

    改善部分显示效果，增加了许多来自不同制作者的MaliciousOS，提供了更多的MaliciousOS下载方式

---
+++ maliciousos/download/script.js
@@ -0,0 +1,40 @@
+var longer_MOS7Div = document.getElementById("longer_MaliciousOS7_Div");
+var longer_MOS10Div = document.getElementById("longer_MaliciousOS10_Div");
+var longer_MOS11Div = document.getElementById("longer_MaliciousOS11_Div");
+var WinSxSNT_MOS8_1Div = document.getElementById("WinSxS-NT_MaliciousOS8_1_Div");
+var _360_MOS11Div = document.getElementById("_360_MaliciousOS11_Div");
+var longer_SYSTEMBtn = document.getElementById("longer_SYSTEM_btn");
+var WinSxSNT_SYSTEMBtn = document.getElementById("WinSxS-NT_SYSTEM_btn");
+var _360_SYSTEMBtn = document.getElementById("_360_SYSTEM_btn");
+function toLongerToggleDiv() {
+    longer_SYSTEMBtn.classList.add("selected");
+    WinSxSNT_SYSTEMBtn.classList.remove("selected");
+    _360_SYSTEMBtn.classList.remove("selected");
+    longer_MOS7Div.style.display = "block";
+    longer_MOS10Div.style.display = "block";
+    longer_MOS11Div.style.display = "block";
+    WinSxSNT_MOS8_1Div.style.display = "none";
+    _360_MOS11Div.style.display = "none";
+}
+
+function toWinSxSNTToggleDiv() {
+    longer_SYSTEMBtn.classList.remove("selected");
+    WinSxSNT_SYSTEMBtn.classList.add("selected");
+    _360_SYSTEMBtn.classList.remove("selected");
+    longer_MOS7Div.style.display = "none";
+    longer_MOS10Div.style.display = "none";
+    longer_MOS11Div.style.display = "none";
+    WinSxSNT_MOS8_1Div.style.display = "block";
+    _360_MOS11Div.style.display = "none";
+}
+
+function to_360_ToggleDiv() {
+    longer_SYSTEMBtn.classList.remove("selected");
+    WinSxSNT_SYSTEMBtn.classList.remove("selected");
+    _360_SYSTEMBtn.classList.add("selected");
+    longer_MOS7Div.style.display = "none";
+    longer_MOS10Div.style.display = "none";
+    longer_MOS11Div.style.display = "none";
+    WinSxSNT_MOS8_1Div.style.display = "none";
+    _360_MOS11Div.style.display = "block";
+}
