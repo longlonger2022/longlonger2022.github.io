a4db3d83c48eff6ba3077bd0d84419cc0923e794
longlonger2022 <long_long2022@163.com>
2025-08-05 20:22:49 +0800

    增加了MaliciousOS的下载方式

--- maliciousos/download/script.js
+++ maliciousos/download/script.js
@@ -3,7 +3,6 @@ var longer_MOS10Div = document.getElementById("longer_MaliciousOS10_Div");
 var longer_MOS11Div = document.getElementById("longer_MaliciousOS11_Div");
 var WinSxSNT_MOS8_1Div = document.getElementById("WinSxS-NT_MaliciousOS8_1_Div");
 var WinSxSNT_MOS10Div = document.getElementById("WinSxS-NT_MaliciousOS10_Div");
-var WinSxSNT_MOS11Div = document.getElementById("WinSxS-NT_MaliciousOS11_Div");
 var _360_MOS7Div = document.getElementById("_360_MaliciousOS7_Div");
 var _360_MOS11Div = document.getElementById("_360_MaliciousOS11_Div");
 var longer_SYSTEMBtn = document.getElementById("longer_SYSTEM_btn");
@@ -18,7 +17,6 @@ function toLongerToggleDiv() {
     longer_MOS11Div.style.display = "block";
     WinSxSNT_MOS8_1Div.style.display = "none";
     WinSxSNT_MOS10Div.style.display = "none";
-    WinSxSNT_MOS11Div.style.display = "none";
     _360_MOS7Div.style.display = "none";
     _360_MOS11Div.style.display = "none";
 }
@@ -32,7 +30,6 @@ function toWinSxSNTToggleDiv() {
     longer_MOS11Div.style.display = "none";
     WinSxSNT_MOS8_1Div.style.display = "block";
     WinSxSNT_MOS10Div.style.display = "block";
-    WinSxSNT_MOS11Div.style.display = "block";
     _360_MOS7Div.style.display = "none";
     _360_MOS11Div.style.display = "none";
 }
@@ -46,7 +43,6 @@ function to_360_ToggleDiv() {
     longer_MOS11Div.style.display = "none";
     WinSxSNT_MOS8_1Div.style.display = "none";
     WinSxSNT_MOS10Div.style.display = "none";
-    WinSxSNT_MOS11Div.style.display = "none";
     _360_MOS7Div.style.display = "block";
     _360_MOS11Div.style.display = "block";
 }
