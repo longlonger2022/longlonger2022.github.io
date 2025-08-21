abfd461984e1a32ee746ed07ffb56187b88d19ac
longlonger2022 <long_long2022@163.com>
2025-08-21 15:34:01 +0800

    更新了万能倒计时，增加了一位作者的MaliciousOS。Updated Universal-Timer and add MaliciousOS by one author.

--- maliciousos/download/script.js
+++ maliciousos/download/script.js
@@ -5,13 +5,16 @@ var WinSxSNT_MOS8_1Div = document.getElementById("WinSxS-NT_MaliciousOS8_1_Div")
 var WinSxSNT_MOS10Div = document.getElementById("WinSxS-NT_MaliciousOS10_Div");
 var _360_MOS7Div = document.getElementById("_360_MaliciousOS7_Div");
 var _360_MOS11Div = document.getElementById("_360_MaliciousOS11_Div");
+var mougou666_MOSXPDiv = document.getElementById("mougou666_MaliciousOSXP_Div");
 var longer_SYSTEMBtn = document.getElementById("longer_SYSTEM_btn");
 var WinSxSNT_SYSTEMBtn = document.getElementById("WinSxS-NT_SYSTEM_btn");
 var _360_SYSTEMBtn = document.getElementById("_360_SYSTEM_btn");
+var mougou666_SYSTEMBtn = document.getElementById("mougou666_SYSTEM_btn");
 function toLongerToggleDiv() {
     longer_SYSTEMBtn.classList.add("selected");
     WinSxSNT_SYSTEMBtn.classList.remove("selected");
     _360_SYSTEMBtn.classList.remove("selected");
+    mougou666_SYSTEMBtn.classList.remove("selected");
     longer_MOS7Div.style.display = "block";
     longer_MOS10Div.style.display = "block";
     longer_MOS11Div.style.display = "block";
@@ -19,12 +22,14 @@ function toLongerToggleDiv() {
     WinSxSNT_MOS10Div.style.display = "none";
     _360_MOS7Div.style.display = "none";
     _360_MOS11Div.style.display = "none";
+    mougou666_MOSXPDiv.style.display = "none";
 }
 
 function toWinSxSNTToggleDiv() {
     longer_SYSTEMBtn.classList.remove("selected");
     WinSxSNT_SYSTEMBtn.classList.add("selected");
     _360_SYSTEMBtn.classList.remove("selected");
+    mougou666_SYSTEMBtn.classList.remove("selected");
     longer_MOS7Div.style.display = "none";
     longer_MOS10Div.style.display = "none";
     longer_MOS11Div.style.display = "none";
@@ -32,12 +37,14 @@ function toWinSxSNTToggleDiv() {
     WinSxSNT_MOS10Div.style.display = "block";
     _360_MOS7Div.style.display = "none";
     _360_MOS11Div.style.display = "none";
+    mougou666_MOSXPDiv.style.display = "none";
 }
 
 function to_360_ToggleDiv() {
     longer_SYSTEMBtn.classList.remove("selected");
     WinSxSNT_SYSTEMBtn.classList.remove("selected");
     _360_SYSTEMBtn.classList.add("selected");
+    mougou666_SYSTEMBtn.classList.remove("selected");
     longer_MOS7Div.style.display = "none";
     longer_MOS10Div.style.display = "none";
     longer_MOS11Div.style.display = "none";
@@ -45,4 +52,20 @@ function to_360_ToggleDiv() {
     WinSxSNT_MOS10Div.style.display = "none";
     _360_MOS7Div.style.display = "block";
     _360_MOS11Div.style.display = "block";
+    mougou666_MOSXPDiv.style.display = "none";
+}
+
+function to_mougou666_ToggleDiv() {
+    longer_SYSTEMBtn.classList.remove("selected");
+    WinSxSNT_SYSTEMBtn.classList.remove("selected");
+    _360_SYSTEMBtn.classList.remove("selected");
+    mougou666_SYSTEMBtn.classList.add("selected");
+    longer_MOS7Div.style.display = "none";
+    longer_MOS10Div.style.display = "none";
+    longer_MOS11Div.style.display = "none";
+    WinSxSNT_MOS8_1Div.style.display = "none";
+    WinSxSNT_MOS10Div.style.display = "none";
+    _360_MOS7Div.style.display = "none";
+    _360_MOS11Div.style.display = "none";
+    mougou666_MOSXPDiv.style.display = "block";
 }
